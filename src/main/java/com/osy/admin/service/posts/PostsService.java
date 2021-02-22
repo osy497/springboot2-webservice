685b13df76909984942dbcc4ab61ce4032c05777
osy497 <osy4997@gmail.com>
2021-02-22 16:25:57 +0900

    Until 5 chapter

--- src/main/java/com/osy/admin/service/posts/PostsService.java
+++ src/main/java/com/osy/admin/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.osy.admin.service.posts;
 
 import com.osy.admin.domain.posts.Posts;
 import com.osy.admin.domain.posts.PostsRepository;
+import com.osy.admin.web.dto.PostsListResponseDto;
 import com.osy.admin.web.dto.PostsResponseDto;
 import com.osy.admin.web.dto.PostsSaveRequestDto;
 import com.osy.admin.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,8 +31,21 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAll().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+
     }
 }
---
+++ src/main/java/com/osy/admin/web/IndexController.java
@@ -0,0 +1,44 @@
+package com.osy.admin.web;
+
+import com.osy.admin.config.auth.dto.SessionUser;
+import com.osy.admin.service.posts.PostsService;
+import com.osy.admin.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        return "index";
+    }
+
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+}
